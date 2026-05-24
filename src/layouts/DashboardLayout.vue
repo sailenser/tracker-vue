3eaa23bf48736b7d4df17387ce8dbefc37c5f93c
baranov.m <baranov.m@promail366.com>
2026-05-24 19:18:32 +0300

    withdrawal of routes

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,9 +1,27 @@
 <template>
   <main class="page-content">
+    <nav>
+      <ul>
+        <li
+            v-for="item in menuItems"
+            :key="item.route"
+        >
+          <router-link
+              :to="{ name: item.route }"
+              class="nav-link"
+              active-class="active"
+          >
+            {{ item.title }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+
     <div class="page-content__wrapper">
       <div class="page-aside page-content__aside">
         Aside
       </div>
+
       <div class="page-content__stage">
         <slot />
       </div>
@@ -11,8 +29,22 @@
   </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue'
+
 defineOptions({
   name: 'DashboardLayout',
 })
+
+interface MenuItem {
+  route: string
+  title: string
+  exact: boolean
+}
+
+const menuItems = ref<MenuItem[]>([
+  { route: 'home', title: 'home', exact: true },
+  { route: 'habits', title: 'habits', exact: true },
+  { route: 'login', title: 'login', exact: true },
+])
 </script>
