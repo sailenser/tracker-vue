de050b8b8c435ec521aeb666ccf288bfe8e17bfb
baranov.m <baranov.m@promail366.com>
2026-05-24 17:41:41 +0300

    add port cleent dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    server: {
+        port: 5174,
+        strictPort: true
     },
-  },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
